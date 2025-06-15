8a290c4cd1fa77749f8ad0d4f5ef8c7bf71e234a
Samir Ortiz <samir.ceccim.ext@dasa.com.br>
2025-06-15 00:57:39 -0300

    Status acessor

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'description', 'status', 'priority'];
+
+    // Acessor
+    public function getStatusAttribute($value)
+    {
+        return $value == 1 ? 'Active' : 'Inactive';
+    }
 }
