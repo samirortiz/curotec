fb1642ba5fd57723b54e445696687b5bf48e5583
Samir Ortiz <samir.ceccim.ext@dasa.com.br>
2025-06-15 00:56:46 -0300

    Requests validations

---
+++ app/Http/Requests/StoreProjectRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'priority' => 'required|integer|min:0|max:9',
+            'status' => 'required|integer|in:0,1'
+        ];
+    }
+}
